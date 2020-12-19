bc678dc0887dcb3bac1f145072708adce0bf556d
dengdejian <3225376756@qq.com>
2020-12-19 10:09:16 +0800

    提交的高德地图

---
+++ gd/main/java/com/example/my_sd_open/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.my_sd_open;
+
+import android.location.Location;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.MyLocationStyle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private AMap aMap;
+
+    private AMapLocationClient mlocationClient;
+    private AMapLocationClientOption mLocationOption;
+    private Location amapLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //定义了一个地图view
+
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        aMap = mapView.getMap();
+
+        //获取地图控件引用
+        mapView = (MapView) findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mapView.onCreate(savedInstanceState);
+
+        initLa();
+    }
+
+    private void initLa() {
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，并且蓝点会跟随设备移动。
+//        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);//只定位一次。
+
+//        aMap.setLocationSource(this, activate(OnLocationChangedListener, listener));
+//        deactivate();
+    }
+
+
+//    private void activate(OnLocationChangedListener listener) {
+//        mListener = listener;
+//        if (mlocationClient == null) {
+//            //初始化定位
+//            mlocationClient = new AMapLocationClient(this);
+//            //初始化定位参数
+//            mLocationOption = new AMapLocationClientOption();
+//            //设置定位回调监听
+//            mlocationClient.setLocationListener((AMapLocationListener) MainActivity.this);
+//            //设置为高精度定位模式
+//            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+//            //设置定位参数
+//            mlocationClient.setLocationOption(mLocationOption);
+//            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+//            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+//            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+//            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+//            mlocationClient.startLocation();//启动定位
+//
+//            onLocationChanged(AMapLocation amapLocation);
+//            mListener.onLocationChanged(amapLocation);
+//        }
+//    }
+
+    /**
+     * 定位成功后回调函数
+     */
+//    public void onLocationChanged(AMapLocation amapLocation) {
+//        if (mListener != null && amapLocation != null) {
+//            if (amapLocation != null
+//                    && amapLocation.getErrorCode() == 0) {
+//                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+//            } else {
+//                String errText = "定位失败," + amapLocation.getErrorCode() + ": " + amapLocation.getErrorInfo();
+//                Log.e("AmapErr", errText);
+//            }
+//        }
+//    }
+
+
+    /**
+     * 停止定位
+     */
+//    public void deactivate() {
+//        mListener = null;
+//        if (mlocationClient != null) {
+//            mlocationClient.stopLocation();
+//            mlocationClient.onDestroy();
+//        }
+//        mlocationClient = null;
+//    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mapView.onSaveInstanceState(outState);
+    }
+}
